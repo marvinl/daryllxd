1adc4c48f455555db150cdee79db78142861e40b
Jedford Seculles <jfjade18@gmail.com>
2015-04-01 05:19:47 +0800

    temp removed "ng"
    TODO: fix words with "ng"

--- lib/daryllxd.rb
+++ lib/daryllxd.rb
@@ -2,10 +2,8 @@ require "daryllxd/version"
 
 class String
   def darify!
-    downcase.scan(/([^aeiou]?[aeiou](ng|[^aeiou](?![aeiou]))?)/).reverse.join.squeeze
+    downcase.scan(/([^aeiou]?[aeiou]([^aeiou](?![aeiou]))?)/).reverse.join.squeeze
   end
 end
 
-module Daryllxd
-  
-end
+module Daryllxd; end
